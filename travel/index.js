ef9c756cce37849738fb5d97f8b6b6c5649aa3a3
Andrei Plavinski <plavinskiandrei@gmail.com>
2022-07-15 18:15:17 +0300

    feat: add pop-up

--- travel/index.js
+++ travel/index.js
@@ -4,6 +4,9 @@ const menu = document.querySelector('.header__nav');
 const menuCloseItem = document.querySelector('.header__nav-close');
 const menuLinks = document.querySelectorAll('.header__link');
 const backDiv = document.querySelector('.back_div');
+const loginButton = document.querySelector('.header__submit');
+const popup = document.querySelector('.pop-up');
+const popupContent = document.querySelector('.pop-up__content')
 
 burgerItem.addEventListener('click', () => {
 	menu.classList.add('header__nav-active');
@@ -22,14 +25,23 @@ for (let i = 0; i < menuLinks.length; i += 1) {
 		backDiv.classList.remove('back_div_on');
 	});
 }
-
-
 backDiv.addEventListener('click', () => {
 	menu.classList.remove('header__nav-active');
 	backDiv.classList.remove('dark_mode_on');
 	backDiv.classList.remove('back_div_on');
 })
 
+loginButton.addEventListener('click', () => {
+	popup.classList.toggle('hidden');
+	popupContent.classList.toggle('pop-up__content-on');
+})
+
+popup.addEventListener('click', (event) => {
+	if (event.target.classList.contains('pop-up')) {
+		popup.classList.toggle('hidden');
+		popupContent.classList.toggle('pop-up__content-on');
+	}
+})
 
 
 
